724fd12df5fc95cb33ed7f0d455a93e71f328175
anisberto reis <anisbertoos@gmail.com>
2022-03-02 22:32:19 -0300

    config hystrix e ajuste de tempo rebbon e hystrix

--- hr-payrol/src/main/java/br/com/anisberto/hrpayroll/resources/PaymentResource.java
+++ hr-payrol/src/main/java/br/com/anisberto/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.anisberto.hrpayroll.entities.Payment;
 import br.com.anisberto.hrpayroll.services.PaymentService;
 
@@ -17,13 +19,20 @@ public class PaymentResource {
 	private PaymentService paymentService;
 
 	@GetMapping
-	public ResponseEntity<String> getStatusServer(){
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
+	public ResponseEntity<String> getStatusServer() {
 		return ResponseEntity.ok().body(new String("Servidor Rodando..."));
 	}
-	
+
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = paymentService.getPayment(workerId, days);
 		return ResponseEntity.ok().body(payment);
+	}
+
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment pay = Payment.builder().name("Dado Mocado").dailyIncome(0.00).days(days).build();
+		return ResponseEntity.ok().body(pay);
 	}
 }
